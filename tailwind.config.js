a6fd8f923c8d3c771ca84984acc90607a5705e88
hoanjackbauer <hoan.jackbauer@gmail.com>
2024-04-07 16:46:03 +0700

    up

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      'ip': '481px',
+      // => @media (min-width: 481px) { ... }
+      
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: [],
+}
